a0f00a693d42f9594aa70073f54f804ccd8b10b3
rajsarky <help.rajsarkar@gmail.com>
2024-02-20 23:52:01 +0000

    yeee!! posts working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     @section('hero')
-        <div class="w-full text-center py-32">
-            <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-                Welcome to <span class="text-yellow-500">German</span> <span class="text-gray-900"> Genie</span>
-            </h1>
-            <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
-            <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block" href="http://localhost/blog">Start
-                Reading</a>
-        </div>
+    <div class="w-full text-center py-32">
+        <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
+            Welcome to <span class="text-yellow-500">German</span> <span class="text-gray-900"> Genie</span>
+        </h1>
+        <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
+        <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block" href="http://localhost/blog">Start
+            Reading</a>
+    </div>
     @endsection
 
 
@@ -16,6 +16,8 @@
             <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
+                    @foreach ($featuredPosts as $post)
+
                     <div class="md:col-span-1 col-span-3">
                         <a href="http://127.0.0.1:8000/blog/laravel-34">
                             <div>
@@ -25,8 +27,7 @@
                         </a>
                         <div class="mt-3">
                             <div class="flex items-center mb-2">
-                                <a href="http://127.0.0.1:8000/categories/laravel"
-                                    class="bg-red-600
+                                <a href="http://127.0.0.1:8000/categories/laravel" class="bg-red-600
                                     text-white
                                     rounded-xl px-3 py-1 text-sm mr-3">
                                     Laravel
@@ -38,10 +39,11 @@
 
                     </div>
 
+                    @endforeach
+
                 </div>
             </div>
-            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
-                href="http://localhost/blog">More
+            <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="http://localhost/blog">More
                 Posts</a>
         </div>
         <hr>
@@ -50,8 +52,7 @@
         <div class="w-full mb-5">
 
         </div>
-        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold"
-            href="http://127.0.0.1:8000/blog">More
+        <a class="mt-10 block text-center text-lg text-yellow-500 font-semibold" href="http://127.0.0.1:8000/blog">More
             Posts</a>
     </div>
 
