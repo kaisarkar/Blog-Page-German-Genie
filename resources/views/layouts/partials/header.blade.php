57f202105274e83d2ca5ede96fb572fb9e811fd1
rajsarky <help.rajsarkar@gmail.com>
2024-02-20 23:19:01 +0000

    database migrations update

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
+    <div id="header-left" class="flex items-center">
+        <div class="text-gray-800 font-semibold">
+            <span class="text-yellow-500 text-xl">German</span> Genie
+        </div>
+        <div class="top-menu ml-10">
+            <ul class="flex space-x-4">
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-900 text-sm text-yellow-500"
+                        href="http://localhost">
+                        Home
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                        href="http://localhost/blog">
+                        Blog
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                        href="http://localhost/blog">
+                        About Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                        href="http://localhost/blog">
+                        Contact Us
+                    </a>
+                </li>
+
+                <li>
+                    <a class="flex space-x-2 items-center hover:text-yellow-500 text-sm text-gray-500"
+                        href="http://localhost/blog">
+                        Terms
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <div id="header-right" class="flex items-center md:space-x-6">
+        @guest
+        @include('layouts.partials.header-right-guest')
+        @endguest
+
+        @auth
+            @include('layouts.partials.header-right-auth')
+        @endauth
+
+    </div>
+</header>
