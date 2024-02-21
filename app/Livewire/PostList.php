1c874c5a1398fe302424ea1af98703007c714ce0
rajsarky <help.rajsarkar@gmail.com>
2024-02-21 14:01:25 +0000

    postlist update liveware

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class PostList extends Component
 {
+
+    #[Computed()]
+    public function posts(){
+        return Post::take(5)->get();
+    }
+
     public function render()
     {
         return view('livewire.post-list');
