fa343b8c9223b932a814961e977534459645659f
rajsarky <help.rajsarkar@gmail.com>
2024-02-21 13:47:07 +0000

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
+//git merge
+
 class Post extends Model
 {
     use HasFactory;
