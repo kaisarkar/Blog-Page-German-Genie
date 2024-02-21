4cb11cc60668175788c28ab3c6bb056f5ddd7efd
rajsarky <help.rajsarkar@gmail.com>
2024-02-21 14:55:41 +0000

    search test works

--- app/Livewire/SearchBox.php
+++ app/Livewire/SearchBox.php
@@ -4,8 +4,17 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+use function Laravel\Prompts\search;
+
 class SearchBox extends Component
 {
+
+    public $search = '';
+
+    public function updatedSearch(){
+        $this->dispatch('search', search: $this->search);
+    }
+
     public function render()
     {
         return view('livewire.search-box');
