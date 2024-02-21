cbc2a613a575b3767335fb31a48fc78a9b53e263
rajsarky <help.rajsarkar@gmail.com>
2024-02-21 14:01:25 +0000

    postlist update liveware

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,3 +1,14 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+<div class=" px-3 lg:px-7 py-6">
+    <div class="flex justify-between items-center border-b border-gray-100">
+        <div class="flex items-center space-x-4 font-light ">
+            <button class="text-gray-500 py-4">Latest</button>
+            <button class="text-gray-900 py-4 border-b border-gray-700">Oldest</button>
+        </div>
+    </div>
+    <div class="py-4">
+        @foreach ($this->posts as $post)
+        <x-posts.post-item :post="$post"/>
+        @endforeach
+
+    </div>
 </div>
