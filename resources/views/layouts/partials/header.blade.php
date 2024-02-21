a3f895c8b38ea245f6618d3c5edfe865c5a77b4e
rajsarky <help.rajsarkar@gmail.com>
2024-02-21 00:37:55 +0000

    update

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,31 +1,3 @@
-<header class="flex items-center justify-between py-3 px-6 border-b border-gray-100">
-    <div id="header-left" class="flex items-center">
-        <a href="{{route('home')}}">
-            <x-application-logo />
-        </a>
-        <div class="top-menu ml-10">
-            <div class="flex space-x-4">
-
-                <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
-                    {{ __('Home') }}
-                </x-nav-link>
-
-                <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                    {{ __('Blog') }}
-                </x-nav-link>
-
-
-            </div>
-        </div>
-    </div>
-    <div id="header-right" class="flex items-center md:space-x-6">
-        @guest
-        @include('layouts.partials.header-right-guest')
-        @endguest
-
-        @auth
-            @include('layouts.partials.header-right-auth')
-        @endauth
-
-    </div>
+<header class="">
+    <livewire:navigation-menu />
 </header>
