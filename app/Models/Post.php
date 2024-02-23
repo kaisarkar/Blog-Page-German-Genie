561de9f93ce0cbfb65b126c707722179dd5ba5ca
rajsarky <help.rajsarkar@gmail.com>
2024-02-23 20:29:37 +0000

    new post uplode an image function was not working properly - so update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 //git merge
@@ -59,5 +60,12 @@ class Post extends Model
         $mins = round(str_word_count($this->body) / 250);
 
         return ($mins < 1) ? 1 : $mins;
+    }
+
+    public function getThumbnailUrl()
+    {
+        $isUrl = str_contains($this->image, 'http');
+
+        return ($isUrl) ? $this->image : Storage::disk('public')->url($this->image);
     }
 }
