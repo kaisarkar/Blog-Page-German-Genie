30f9a779beb31182be4f26451d0497009f7d47fc
rajsarky <help.rajsarkar@gmail.com>
2024-02-23 21:28:28 +0000

    colour choice for badge

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,4 +1,31 @@
-<a href="#" class="bg-red-600
-                        text-white
+@props(['textColour', 'bgColour'])
+@php
+    $textColour = match ($textColour) {
+        'gray' => 'text-gray-800',
+        'blue' => 'text-blue-800',
+        'red' => 'text-red-800',
+        'yellow' => 'text-yellow-800',
+        'pink' => 'text-pink-800',
+        'indigo' => 'text-indigo-800',
+        'purple' => 'text-purple-800',
+        'green' => 'text-green-800',
+        'lime' => 'text-lime-800',
+        default => 'text-gray-800',
+    };
+
+    $bgColour = match ($bgColour) {
+        'gray' => 'bg-gray-100',
+        'blue' => 'bg-blue-100',
+        'red' => 'bg-red-100',
+        'yellow' => 'bg-yellow-100',
+        'pink' => 'bg-pink-100',
+        'indigo' => 'bg-indigo-100',
+        'purple' => 'bg-purple-100',
+        'green' => 'bg-green-100',
+        'lime' => 'bg-lime-100',
+        default => 'bg-gray-100',
+    };
+@endphp
+<a href="#" class="{{ $textColour }} {{ $bgColour }} text-white
                         rounded-xl px-3 py-1 text-base">
     {{$slot}}</a>
--- resources/views/posts/partials/categories-box.blade.php
+++ resources/views/posts/partials/categories-box.blade.php
@@ -2,7 +2,7 @@
     <h3 class="mb-3 text-lg font-semibold text-gray-900">Recommended Topics</h3>
     <div class="flex flex-wrap justify-start gap-2 topics">
         @foreach ($categories as $category)
-        <x-badge>{{$category->title}}</x-badge>
+        <x-badge textColour="indigo" bgColour="blue">{{$category->title}}</x-badge>
         @endforeach
     </div>
 </div>
