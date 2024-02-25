e339a49bfeccaf2dc686ab63409325ff7abe9a2a
rajsarky <help.rajsarkar@gmail.com>
2024-02-25 00:43:41 +0000

    working on post page - need to work on comments

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
 <div class="text-gray-800 font-semibold">
-    <span class="text-yellow-500 text-3xl">German</span> Genie
+    <span class="text-blue-500 text-3xl">German</span> Genie
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     @section('hero')
     <div class="w-full text-center py-32">
         <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-            Welcome to <span class="text-yellow-500">German</span> <span class="text-gray-900"> Genie</span>
+            Welcome to <span class="text-blue-500">German</span> <span class="text-gray-900"> Genie</span>
         </h1>
         <p class="text-gray-500 text-lg mt-1">Best Blog in the universe</p>
         <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block" href="http://localhost/blog">Start
@@ -13,7 +13,7 @@
 
     <div class="mb-10 w-full">
         <div class="mb-16">
-            <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Featured Posts</h2>
+            <h2 class="mt-16 mb-5 text-3xl text-blue-500 font-bold">Featured Posts</h2>
             <div class="w-full">
                 <div class="grid grid-cols-3 gap-10 w-full">
                     @foreach ($featuredPosts as $post)
@@ -26,7 +26,7 @@
         </div>
         <hr>
 
-        <h2 class="mt-16 mb-5 text-3xl text-yellow-500 font-bold">Latest Posts</h2>
+        <h2 class="mt-16 mb-5 text-3xl text-blue-500 font-bold">Latest Posts</h2>
         <div class="w-full mb-5">
             <div class="grid grid-cols-3 gap-10 w-full">
                 @foreach ($latestPosts as $post)
