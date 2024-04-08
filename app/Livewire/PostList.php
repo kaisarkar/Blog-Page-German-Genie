8958d67723bb793e5bc09fceff2eb6ea5d953e2e
rajsarky <help.rajsarkar@gmail.com>
2024-04-08 21:24:50 +0100

    Checkbox added for most liked post

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -22,6 +22,8 @@ class PostList extends Component
     public $search = '';
     #[Url()]
     public $category = '';
+    #[Url()]
+    public $popular = false;
 
     public function setSort($sort)
     {
@@ -45,11 +47,14 @@ class PostList extends Component
     public function posts()
     {
         return Post::published()
-        ->orderBy('published_at', $this->sort)
         ->when($this->activeCategory, function ($query) {
             $query->withCategory($this->category);
         })
+        ->when($this->popular, function ($query) {
+            $query->withCount('likes')-> orderBy("likes_count", 'desc');
+        })
         ->where('title', 'like', "%{$this->search}%")
+        ->orderBy('published_at', $this->sort)
         ->paginate(5);
     }
 
